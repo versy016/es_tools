b01d81dbb9701ad30ee9597af8cdf2f7db0c7d77
Shivam Verma <sverma@engsurveys.com.au>
2024-06-04 15:35:40 +0930

    added authentication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,45 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "estools": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    },
+    "estoolsapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "estools": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "estoolslambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "estoolsdb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import React, { useEffect, useState } from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import { ThemeProvider } from '@aws-amplify/ui-react';
+import amplifyconfig from './amplifyconfiguration.json';
+import studioTheme from './ui-components/studioTheme';
 
-function App() {
+Amplify.configure(amplifyconfig);
+
+const styles = {
+  container: {
+    padding: '20px',
+  },
+};
+
+const App = ({ signOut }) => {
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        const attributes = await fetchUserAttributes();
+        setUserName(attributes.name);
+      } catch (error) {
+        console.error(error);
+        setUserName('User');
+      }
+    };
+    fetchUserName();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <Heading level={1}>Hello {userName || 'Loading...'}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+      <h2>ES Tools</h2>
     </div>
   );
-}
+};
 
-export default App;
+const AppWithAuth = () => (
+  <div style={styles.container}>
+    <Authenticator >
+      {({ signOut }) => (
+        <ThemeProvider theme={studioTheme}>
+          <App signOut={signOut} />
+        </ThemeProvider>
+      )}
+    </Authenticator>
+  </div>
+);
+
+export default AppWithAuth;
